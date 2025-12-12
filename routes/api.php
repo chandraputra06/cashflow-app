f708a693cbfa6fe8d268ca7f37f2f027cca7f0ce
chandraputra06 <chandra.wisesa06@gmail.com>
2025-12-12 22:27:37 +0800

    feat(category): create  comment in api route

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::resource('categories', CategoryController::class);
 
 Route::get('categories/relation', [CategoryController::class, 'relation'])->name('categories.relation');
+
+//masuk resource
